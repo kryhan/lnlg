f4717ede9927fc02a4548f83ba89751cf421cc15
hanqianwen <hanyouxiu@163.com>
2022-09-24 21:47:47 +0800

    002

--- 项目1/js/main.js
+++ 项目1/js/main.js
@@ -1,13 +1,3 @@
-/*整个站点的js脚本*/
-//封装好的Promise,异步请求推荐使用该方法，只需要编写一次
-/*使用方式
-getData('get|post','url').then(function (数据) {
-    成功之后的业务处理逻辑
-},function (errorInfo) {
-    失败之后的业务处理逻辑
-});
-*/
-
 let getData = function(method,url){
     let promise = new Promise(function(resolve, reject){
         let xhr = new XMLHttpRequest();
@@ -32,5 +22,57 @@ let getData = function(method,url){
 };
 
 function gotoPage(event){
+    event.stopPropagation();
+    event.preventDefault();
     window.location.href = event.target.getAttribute("data-href");
+}
+//用户名称检查
+function checkUsername(event){
+    let flag = false;
+    if(event.target.value.length >= 3){
+        //访问服务器获取到查重的结果
+        getData('get','./data/userCheck.json').then(function (myData) {
+            let str='';
+            if(!myData.statusCode){//成功提示
+                str = '恭喜，该用户名可用';
+            }else{
+                str = '抱歉，该用户名不可用';
+            }
+            let oTips = document.createElement('div');
+            let oText = document.createTextNode(str);
+            oTips.append(oText);
+            event.target.parentElement.append(oTips);
+        },function (errorInfo) {
+            alert('访问失败');
+        });
+    }else{
+        //alert('用户名长度至少3个字母');
+    }
+    return flag;
+}
+
+//获取所有商家数据
+function getAllBusiness(){
+    getData('get','./data/businessList.json').then(function (myData) {
+        if(!(oUl = document.querySelector('.businessList'))) return false;
+        let sStr = ``;
+        for (let i = 0; i < myData.lists.length; i++) {
+            sStr += `
+                <li>
+                    <a href="${myData.lists[i].url}">
+                        <p>
+                            <img src="${myData.lists[i].imgsrc}" />
+                            <span>${myData.lists[i].tips}</span>
+                        </p>
+                        <h3>${myData.lists[i].title}</h3>
+                        <div>${myData.lists[i].role.join(' | ')}</div>
+                        <div>${myData.lists[i].description}</div>
+                    </a>
+                </li>
+            `;
+        }
+        oUl.innerHTML = sStr;
+    },function (errorInfo) {
+        alert("失败之后的业务处理逻辑");
+    });
 }
